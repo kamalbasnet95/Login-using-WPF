58737bf26e61a96ef9febf8149d6b9b30742e570
kamalbasnet95 <60617099+kamalbasnet95@users.noreply.github.com>
2020-02-28 18:53:44 -0500

    Add files via upload

---
+++ Payroll System/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Payroll_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+         
+        }
+        
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+           
+            //Window1 useracct = new Window1 { Owner = this };
+            //useracct.ShowInTaskbar = false;
+            ///useracct.Owner = Application.Current.MainWindow;
+           // useracct.Show();
+          
+
+        }
+    }
+}
